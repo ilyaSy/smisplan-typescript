5f06a4771836a46617a637452469bf47b2f5faf3
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-12-28 23:23:48 +0300

    Refac: remove comments

--- src/components/UI/SuspenseFallback/SuspenseFallback.test.js
+++ src/components/UI/SuspenseFallback/SuspenseFallback.test.js
@@ -7,10 +7,7 @@ describe('SuspenseFallback', () => {
   test('Loading', () => {
     const { container } = render(<SuspenseFallback type="loading"/>);
 
-    // expect(container.firstChild.classList.contains('ant-spin ant-spin-spinning loading-component')).toBe(true);
-    // expect(screen.getBy('svg')).toBe(true);
     expect(container.firstChild?.classList.contains('loading-component')).toBe(true);
-    // expect(screen.getByRole('svg')).toMatchInlineSnapshot(renderer.create(<LoadingComponent />));
   })
 
   test('Text node', () => {
