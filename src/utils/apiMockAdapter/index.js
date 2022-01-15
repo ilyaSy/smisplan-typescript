54a5adeb836670344e440f4a8d888f0b0b65a9d6
Ilya Sychugov <ilya.sychugov@gmail.com>
2022-01-15 03:38:54 +0300

    Refac

--- src/utils/apiMockAdapter/index.js
+++ src/utils/apiMockAdapter/index.js
@@ -46,47 +46,35 @@ const replyPostWithOK = (mode) => (mockResponseConfig) => {
 export default function setMockAdapter() {
   // get basic information
   mock.onGet(`${urlApi}/user/`).reply(200, user);
-
   mock.onGet(`${urlApi}/developer/`).reply(200, developers);
-
   mock.onGet(`${urlApi}/project/`).reply(200, projects);
 
   // get meta data information
   mock.onGet(`${urlApi}/task-meta/`).reply(200, metaData.task);
-
   mock.onGet(`${urlApi}/discussion-meta/`).reply(200, metaData.discussion);
-
   mock.onGet(`${urlApi}/calendar-meta/`).reply(200, metaData.calendar);
-
   mock.onGet(`${urlApi}/event-meta/`).reply(200, metaData.event);
 
   // get data
   mock.onGet(`${urlApi}/task/`).reply(200, data.task);
-
   mock.onGet(`${urlApi}/discussion/`).reply(200, data.discussion);
-
   mock.onGet(`${urlApi}/calendar/`).reply(200, data.calendar);
-
   mock.onGet(`${urlApi}/event/`).reply(200, data.event);
 
   // put data
-  mock.onPut(`${urlApi}/task/`).reply(replyPostWithOK("task"));
   // mock.onPut(`${urlApi}/task/`).reply(404);
-
-  mock.onPut(`${urlApi}/discussion/`).reply(200, { status: 'OK', error: '', data: { id: 777 } });
+  mock.onPut(`${urlApi}/task/`).reply(replyPostWithOK("task"));
+  mock.onPut(`${urlApi}/discussion/`).reply(replyPostWithOK("discussion"));
 
   // patch data
   mock.onPatch(`${urlApi}/task/`).reply(replyPostWithOK("task"));
-
-  mock.onPatch(`${urlApi}/discussion/`).reply(200, { status: 'OK', error: '', data: {} });
+  mock.onPatch(`${urlApi}/discussion/`).reply(replyPostWithOK("discussion"));
 
   // delete data
   mock.onDelete(`${urlApi}/task/`).reply(replyPostWithOK("task"));
-
-  mock.onDelete(`${urlApi}/discussion/`).reply(200, { status: 'OK', error: '' });
+  mock.onDelete(`${urlApi}/discussion/`).reply(replyPostWithOK("discussion"));
 
   // post data
   mock.onPost(`${urlApi}/task/`).reply(200, { status: 'OK', error: '' });
-
   mock.onPost(`${urlApi}/discussion/`).reply(200, { status: 'OK', error: '' });
 }
